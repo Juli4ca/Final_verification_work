0d9606de5a12e820a22f8ab295a32a5d9ea16150
Yuliya <sirena_travel@mail.ru>
2022-09-19 16:13:54 +0300

    method Print

--- Quarter_1/Program.cs
+++ Quarter_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// test
-Console.WriteLine("Hello, World!");
+﻿// Дан одномерный массив строк. Программа формирует из имеющегося массива, массив из строк,
+// длина которых меньше или равна 3 символам. 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
