4dd7c0d605664fcad9d09f3c2e1ad4fb4a04095d
Yuliya <sirena_travel@mail.ru>
2022-09-19 16:54:37 +0300

    method MassStr

--- Quarter_1/Program.cs
+++ Quarter_1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Дан одномерный массив строк. Программа формирует из имеющегося массива, массив из строк,
 // длина которых меньше или равна 3 символам. 
-void Print(int[] arr)
+void Print(string[] arr)
 {
     int size = arr.Length;
 
@@ -11,3 +11,18 @@ void Print(int[] arr)
     Console.WriteLine();
 }
 
+string[] MassStr (int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {size} string: ");
+        arr[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return arr;
+        
+}
+
+string[] arr_1 = MassStr(5);
+Print(arr_1);
